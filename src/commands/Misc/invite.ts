a3da6a328342c04cc9f4356e324db3ba80591c0f
Usbo <72393528+UsboKirishima@users.noreply.github.com>
2021-08-02 09:56:50 +0200

    Update invite.ts

--- src/commands/Misc/invite.ts
+++ src/commands/Misc/invite.ts
@@ -9,7 +9,7 @@ import { UserFlagsString } from "discord.js";
 export async function run (client: ReknownClient, message: Message, args: string[]) {
     let embed: any = new MessageEmbed()
     .setAuthor(client.user?.tag, client.user?.displayAvatarURL())
-    .setDescription('[INVITE](https://www.dsc.gg/niki-bot) | [SUPPORT SERVER](https://www.dsc.gg/MagicPoison)')
+    .setDescription('[INVITE](https://discord.com/api/oauth2/authorize?client_id=848616509765189642&permissions=8&scope=bot%20applications.commands) | [SUPPORT SERVER](https://www.dsc.gg/MagicPoison)')
     .setColor('RANDOM')
     message.channel.send(embed)
 }
